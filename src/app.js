bb75756b18f520079a2ef669a7ef5545d972850c
gonzalogiollo <gonzalogiollo@gmail.com>
2023-05-04 10:32:34 -0300

    feat: tercera parte chat

--- src/app.js
+++ src/app.js
@@ -17,4 +17,20 @@ app.use('/', viewsRouter);
 const server = app.listen(8080, () => console.log('Server running'));
 const io = new Server(server);
 
+const messages = [];
+
+io.on('connection', socket=>{
+    console.log('nuevo cliente conectado')
+
+    socket.on('message', data=>{
+        messages.push(data);
+        io.emit('messageLogs', messages);
+    })
+})
+
+
+socket.on('authenticate', data =>{
+    socket.emit('messageLogs', messages);
+    socket.broadcast.emit('newUswerConnected', data)
+})
 
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -17,7 +17,37 @@ Swal.fire({
         return !value && "necesitas escribir un nombre de user para chatear"
     },
     allowOutsideClick: false, //esto hace que si se clickea por afuera del modal, el modal queda esperando que se complete el campo del value.
-    
+    allowEscapekey: false, // esto hace que si dan escape tampoco se salga el modal.
 }).then(result => { //aca hacemos el .then para que sea una promesa y poder enviar el value del result al user que definimos como variable. 
     user = result.value
+    socket.emit('authenticated', user); //el usuario se autentica
+})
+
+chatbox.addEventListener('keyup', evt =>{
+    if(evt.key==='Enter'){
+        if(chatbox.value.trim().length > 0){
+            socket.emit('message', {user, message: chatbox.value})
+        }
+    }
+
+})
+
+socket.on('messageLogs', data=>{
+    let log = document.getElementById('messageLogs')
+    let messages = '';
+    data.forEach(message =>{
+        messages += `${message.user} dice: ${message.message}<br>`
+    });
+    log.innerHTML=messages;
+})
+
+socket.on('newUserConnected', data =>{
+    swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmation: false,
+        timer: 3000,
+        title: `${data} se ha unido al chat`,
+        icon: 'success'
+    })
 })
